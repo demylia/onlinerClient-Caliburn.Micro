5eb7efe7929b63def2c24f3fc99436320ab502c0
Dzmitry_Tsikhanau1 <Dzmitry_Tsikhanau1@epam.com>
2015-10-30 16:36:28 +0300

    add serialization and search

--- OnlinerNews2/ViewModels/MainViewModel.cs
+++ OnlinerNews2/ViewModels/MainViewModel.cs
@@ -4,20 +4,24 @@ using OnlinerServices;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Windows.Storage;
 
 namespace OnlinerNews2.ViewModels
 {
     public class MainViewModel : Screen
     {
         private readonly INavigationService _navigationService;
+      
         private ObservableCollection<NewsItem> news;
         private IDataManager _dataManager;
+        private string textSearch;
 
-       
         public MainViewModel(INavigationService navigationService, IDataManager dataManager)
         {
             
@@ -37,19 +41,78 @@ namespace OnlinerNews2.ViewModels
             }
         }
 
+        public string TextSearch
+        {
+            get { return textSearch; }
+            set
+            {
+                textSearch = value;
+                NotifyOfPropertyChange(() => TextSearch);
+            }
+        }
+
         //Methods
+        protected override async void OnActivate()
+        {
+            News = await ReadDataAsync();
+            if (News == null)
+            {
+                GetNews();
+                await WriteDataAsync();
+            }
+           
+           
+        }
+       private async void GetNews()
+        {
+            string adress = "http://tech.onliner.by/feed";
+            News = new ObservableCollection<NewsItem>(await _dataManager.GetNewsAsync(adress));
+
+        }
         public void GoToDetail(NewsItem item)
         {
             _navigationService.NavigateToViewModel<DetailViewModel>(item);
-       
         }
 
-        protected override async void OnActivate()
+        public async void Search()
         {
-            string adress = "http://tech.onliner.by/feed";
-           
-            News = new ObservableCollection<NewsItem>(await _dataManager.GetNewsAsync(adress));
+            if (string.IsNullOrEmpty(TextSearch))
+            {
+              News = await  ReadDataAsync();
+            }
+            else
+            {
+                var res = await ReadDataAsync();
+                News = new ObservableCollection<NewsItem>(res.Where(s => s.Title.IndexOf(TextSearch,StringComparison.OrdinalIgnoreCase) >= 0));
+              
+            }
+   
+        }
+        public void RefreshNews()
+        {
+            GetNews();
         }
         
+        private async Task WriteDataAsync()
+        {
+            var dcs = new DataContractSerializer(typeof(List<NewsItem>));
+
+            using (var stream = await ApplicationData.Current.LocalFolder.OpenStreamForWriteAsync("news.dat", CreationCollisionOption.ReplaceExisting))
+            {
+                dcs.WriteObject(stream, News);
+            }
+
+        }
+        private async Task<ObservableCollection<NewsItem>> ReadDataAsync()
+        {
+            
+            var myStream = await ApplicationData.Current.LocalFolder.OpenStreamForReadAsync("news.dat");
+            DataContractSerializer dcs = new DataContractSerializer(typeof(List<NewsItem>));
+
+           return new ObservableCollection<NewsItem>((IEnumerable<NewsItem>)dcs.ReadObject(myStream));
+
+        }
+
+       
     }
 }
